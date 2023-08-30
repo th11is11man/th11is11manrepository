a1892352d6b788a93cfe88efa13eb7f62b84c562
th11is11man <137316212+th11is11man@users.noreply.github.com>
2023-08-30 20:58:53 +0500

    Add files via upload

---
+++ app_lesson_4/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+def lesson_4(request):
+    return HttpResponse('Домашка по 4 занятию')
+
+
+# Create your views here.
